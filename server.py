3f86ea964a61bdc81d9e6bdc4c1b0f059dada8e5
Edward Ji <jiziao6@gmail.com>
2021-11-10 01:54:06 +1100

    Switch to polling
    Add user functionalities

--- server.py
+++ server.py
@@ -1,15 +1,221 @@
 #!/bin/python
-import hashlib
 import logging
 import os
-import select
+import selectors
 import signal
 import socket
 import sys
 
+from hashlib import pbkdf2_hmac
+from typing import Callable, List, Optional
 
-def handle(msg):
-    return msg
+
+logging.basicConfig(level=logging.DEBUG)
+
+selector: selectors.BaseSelector = selectors.DefaultSelector()
+
+
+class User:
+
+    instances: List["User"] = []
+
+    def __init__(self, name: str, passwd: str):
+        self.name: str = name
+        self._salt: bytes = os.urandom(16)
+        self._passhash: bytes = pbkdf2_hmac("sha256",
+                                            passwd.encode(),
+                                            self._salt,
+                                            1000)
+        self.logged_in: bool = False
+        self.instances.append(self)
+
+    @classmethod
+    def register(cls, name: str, passwd: str) -> Optional["User"]:
+        for user in cls.instances:
+            if user.name == name:
+                return None
+        user: "User" = User(name, passwd)
+        user.logged_in = True
+        return user
+
+    def login(self, passwd: str) -> bool:
+        if self.logged_in:
+            return False
+        passhash: bytes = pbkdf2_hmac("sha256",
+                                      passwd.encode(),
+                                      self._salt,
+                                      1000)
+        if self._passhash == passhash:
+            self.logged_in = True
+        return self.logged_in
+
+    def logout(self):
+        self.logged_in = False
+
+
+class Session:
+
+    def __init__(self):
+        self._user: User = None
+        self.pending: List[str] = []
+        self.replies: List[str] = []
+
+    def switch_user(self, user: User):
+        self.logout()
+        self._user = user
+
+    def register(self, name: str, passwd: str) -> bool:
+        user = User.register(name, passwd)
+        if user is not None:
+            self.switch_user(user)
+            return True
+        return False
+
+    def login(self, name: str, passwd: str) -> bool:
+        for user in User.instances:
+            if user.name == name:
+                if user.login(passwd):
+                    self.switch_user(user)
+                    return True
+                break
+        return False
+
+    def logout(self):
+        if self._user is not None:
+            self._user.logout()
+
+    def handle(self):
+        while self.pending:
+            self.replies.append(handle(self, self.pending.pop()))
+
+
+def check_n_args(*n_args: int) -> Callable:
+    min_n_args: int = None
+    max_n_args: int = None
+    if len(n_args) == 0:
+        max_n_args = 0
+    elif len(n_args) == 1:
+        min_n_args, = max_n_args, = n_args
+    elif len(n_args) == 2:
+        min_n_args, max_n_args = n_args
+
+    def decorator(func: Callable) -> Callable:
+        def wrapper(session: Session, tokens: List[str]) -> str:
+            if min_n_args is not None and len(tokens) < min_n_args:
+                return "ERROR not enough arguments"
+            if max_n_args is not None and len(tokens) > max_n_args:
+                return "ERROR too many arguments"
+            return func(session, tokens)
+        return wrapper
+    return decorator
+
+
+@check_n_args(2)
+def register(session: Session, tokens: List[str]) -> str:
+    return int(session.register(*tokens))
+
+
+@check_n_args(2)
+def login(session: Session, tokens: List[str]) -> str:
+    return int(session.login(*tokens))
+
+
+@check_n_args(1)
+def join(session: Session, tokens: List[str]) -> str:
+    return "join"
+
+
+@check_n_args(1)
+def create(session: Session, tokens: List[str]) -> str:
+    return "create"
+
+
+@check_n_args(2, None)
+def say(session: Session, tokens: List[str]) -> str:
+    return "say"
+
+
+@check_n_args()
+def channels(session: Session, tokens: List[str]) -> str:
+    return "channels"
+
+
+def handle(session: Session, msg: str) -> str:
+    tokens: List[str] = msg.strip().split()
+
+    if not msg:
+        return "RESULT ERROR empty message"
+
+    msg_type: str = tokens.pop(0)
+    result: str = ""
+    if msg_type == "REGISTER":
+        result = register(session, tokens)
+    elif msg_type == "LOGIN":
+        result = login(session, tokens)
+    elif msg_type == "JOIN":
+        result = join(session, tokens)
+    elif msg_type == "CREATE":
+        result = create(session, tokens)
+    elif msg_type == "SAY":
+        result = say(session, tokens)
+    elif msg_type == "CHANNELS":
+        result = channels(session, tokens)
+    else:
+        return f"RESULT ERROR unrecognised message type {msg_type}"
+
+    return f"RESULT {result}"
+
+
+def accept(server: socket.socket) -> Session:
+    conn, addr = server.accept()
+    logging.info(f"Accept connection from address {addr}")
+    conn.setblocking(False)
+    session = Session()
+    selector.register(conn, selectors.EVENT_READ, session)
+    return session
+
+
+def read(key):
+    conn = key.fileobj
+    session = key.data
+    try:
+        msg = conn.recv(1024).decode()
+        if msg:
+            logging.debug(f"Recieved {msg!r} from {conn.getpeername()}")
+            session.pending.append(msg)
+            selector.modify(conn, selectors.EVENT_WRITE, key.data)
+        else:
+            close(key)
+    except UnicodeError:
+        logging.warning(f"Can not decode message from {conn.getpeername()}")
+    except OSError as e:
+        logging.warning(f"OSError: {e}")
+        session.logout()
+        selector.unregister(conn)
+
+
+def write(key):
+    conn = key.fileobj
+    session = key.data
+    try:
+        while session.replies:
+            reply: str = session.replies.pop(0) + "\n"
+            logging.debug(f"Sending {reply!r} to {conn.getpeername()}")
+            conn.send(reply.encode())
+        selector.modify(conn, selectors.EVENT_READ, key.data)
+    except OSError as e:
+        logging.warning(f"OSError: {e}")
+        session.logout()
+        selector.unregister(conn)
+
+
+def close(key):
+    conn = key.fileobj
+    session = key.data
+    logging.info(f"Closing connection to {conn.getpeername()}")
+    session.logout()
+    selector.unregister(conn)
+    conn.close()
 
 
 # Use this variable for your loop
@@ -20,83 +226,39 @@ loop = True
 def quit_gracefully(signum, frame):
     global loop
     loop = False
-    logging.info("Interrupt triggered")
+    logging.info("Recieved interrupt signal")
 
 
 def main():
-    """
-    The main program setups the server and manages incoming and outgoing
-    socket communications. It handles interrupt gracefullly and closes server
-    before exiting the program.
-    """
     signal.signal(signal.SIGINT, quit_gracefully)
-
-    logging.basicConfig(level=logging.DEBUG)
 
     port = int(sys.argv[1])
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.setblocking(False)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.bind(("localhost", port))
     server.listen()
-    rlist = [server]
-    wlist = []
-    session = {}
+    selector.register(server, selectors.EVENT_READ, None)
+
+    sessions = []
 
     while loop:
-        readable, writable, exceptional = select.select(rlist, wlist, rlist, 1)
-        for s in readable:
-            if s is server:
-                conn, addr = s.accept()
-                conn.setblocking(False)
-                rlist.append(conn)
-                session[conn] = []
-                logging.info(f"Accepted connection from address {addr}")
-            else:
-                try:
-                    data = s.recv(1024)
-                    if data:
-                        logging.debug(f"Recieved data {data}")
-                        session[s].append(data)
-                        if s not in wlist:
-                            wlist.append(s)
-                    else:
-                        if s in wlist:
-                            wlist.remove(s)
-                        if s in writable:
-                            writable.remove(s)
-                        rlist.remove(s)
-                        logging.info(f"Closing connection on {s.getpeername()}")
-                        s.close()
-                        session.pop(s)
-                except OSError as e:
-                    logging.warning(e)
+        events = selector.select(1)
+        for key, mask in events:
+            if key.data is None:
+                sessions.append(accept(server))
+            elif mask & selectors.EVENT_READ:
+                read(key)
+            elif mask & selectors.EVENT_WRITE:
+                write(key)
 
-        for s in writable:
-            try:
-                if session[s]:
-                    result = handle(session[s].pop())
-                    wlist.remove(s)
-                    logging.debug(f"Sending {result} to {s.getpeername()}")
-                    s.send(result)
-            except OSError as e:
-                logging.warning(e)
-                if s in rlist:
-                    rlist.remove(s)
-                session.pop(s)
-
-        for s in exceptional:
-            logging.info(f"Closing connection on {s.address} with exceptional "
-                         "condition")
-            rlist.remove(s)
-            if s in wlist:
-                wlist.remove(s)
-            s.close()
-            session.pop(s)
+        for session in sessions:
+            session.handle()
 
     logging.info("Closing the server")
     server.close()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
