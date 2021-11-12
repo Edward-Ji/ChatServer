5eee9509640cb66572a50a69d4e6da74745306fb
Edward Ji <jiziao6@gmail.com>
2021-11-13 00:05:41 +1100

    Fix client unresponsive

--- client.py
+++ client.py
@@ -12,24 +12,28 @@ def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(("localhost", server_port))
     client.setblocking(False)
-    selector.register(client, selectors.EVENT_READ)
+    selector.register(client, selectors.EVENT_READ | selectors.EVENT_WRITE)
 
     selector.register(sys.stdin, selectors.EVENT_READ)
+
+    pending = []
 
     try:
         while True:
             events = selector.select(1)
             for key, mask in events:
                 if key.fileobj is sys.stdin:
-                    line = sys.stdin.readline()
-                    client.send(line.encode())
+                    pending.append(sys.stdin.readline())
                 if key.fileobj is client:
-                    sys.stdout.write(client.recv(1024).decode())
+                    if mask & selectors.EVENT_READ:
+                        sys.stdout.write(client.recv(1024).decode())
+                    if mask & selectors.EVENT_WRITE:
+                        while pending:
+                            client.send(pending.pop(0).encode())
     except KeyboardInterrupt:
         client.close()
     except Exception as e:
         sys.stdout.write(f"{type(e).__name__}: {e}\n")
-        sys.exit(e.errno)
 
 
 if __name__ == '__main__':
